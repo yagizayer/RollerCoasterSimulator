1cdfa0ac3dc7ebe84df2093bae29ea7d30ea6c32
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-07-25 16:23:21 +0300

    Update TriggerManager.cs

--- Assets/Scripts/TriggerManager.cs
+++ Assets/Scripts/TriggerManager.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using RotaryHeart.Lib.SerializableDictionary;
 using UnityEngine;
+
+[System.Serializable]
+class TriggerAreasDict : SerializableDictionaryBase<int, BoxCollider> { }
 
 public class TriggerManager : MonoBehaviour
 {
