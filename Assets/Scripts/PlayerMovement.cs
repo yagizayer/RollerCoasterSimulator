e0fc076343756c2f17b2a3703e2df7b128e648f0
yagizayer <40693974+yagizayer@users.noreply.github.com>
2021-07-25 17:16:52 +0300

    Add Player Animatoins

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,22 @@ using Helper;
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
     [SerializeField] private NavMeshAgent _navMeshAgent;
     [SerializeField, Range(0.001f, 10f)] private float _movementOffset;
     private Transform _mainCam;
 
     private void Start()
     {
-        if (_navMeshAgent != null) _navMeshAgent = GetComponent<NavMeshAgent>();
+        if (_animator == null) _animator = GetComponentInChildren<Animator>();
+        if (_navMeshAgent == null) _navMeshAgent = GetComponent<NavMeshAgent>();
         _mainCam = Camera.main.transform;
     }
     private void Update()
     {
         Vector3 moveDir = GetInputs().RelativeToCamera(_mainCam);
+        if (moveDir.magnitude >= 0) _animator.SetBool("Moving", true);
+        if (moveDir.magnitude <= 0) _animator.SetBool("Moving", false);
         _navMeshAgent.SetDestination(transform.position + moveDir * _movementOffset);
     }
 
